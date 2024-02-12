c40b0dae6cf9f99a5612a644adfbf3d1bc87c4fd
mitchellshera <mitchellwachera@gmail.com>
2024-02-12 15:28:57 +0300

    updated getConnect method

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,29 +4,36 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
-  static async getConnect(req, res) {
-    const authHeader = req.header('Authorization');
+  static async getConnect(request, response) {
+    const authData = request.header('Authorization');
 
-    if (!authHeader || !authHeader.startsWith('Basic ')) {
-      return res.status(401).json({ error: 'Invalid credentials format' });
+    if (!authData || !authData.startsWith('Basic ')) {
+      response.status(401).json({ error: 'Invalid credentials format' });
+      return;
     }
 
-    const [email, password] = Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString()
-      .split(':');
+    const base64Credentials = authData.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      response.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
 
     const hashedPassword = sha1(password);
-    const user = await dbClient.getUser({ email, password: hashedPassword });
 
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const token = uuidv4();
-    const key = `auth_${token}`;
-    await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
-
-    return res.status(200).json({ token });
+    const users = dbClient.db.collection('users');
+    users.findOne({ email, password: hashedPassword }, async (err, user) => {
+      if (user) {
+        const token = uuidv4();
+        const key = `auth_${token}`;
+        await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+        response.status(200).json({ token });
+      } else {
+        response.status(401).json({ error: 'Unauthorized' });
+      }
+    });
   }
 
   static async getDisconnect(req, res) {
