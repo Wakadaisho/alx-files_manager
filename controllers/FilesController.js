a1a558f413cca64e896ace54d46a463923f478bd
mitchellshera <mitchellwachera@gmail.com>
2024-02-12 19:17:37 +0300

    8

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,17 @@
+// controllers/FilesController.js
+
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
+import mime from 'mime-types';
 import { ObjectId } from 'mongodb';
+import imageThumbnail from 'image-thumbnail';
+import Bull from 'bull';
 
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+
+// Create Bull queue
+const fileQueue = new Bull('fileQueue');
 
 class FilesController {
   static getFileTypes() {
@@ -46,18 +54,28 @@ class FilesController {
       }
     }
 
-    if (type !== 'folder') {
-      fs.mkdirSync(FOLDER_PATH, { recursive: true });
-      fs.writeFileSync(`${FOLDER_PATH}/${uuidv4()}`, data, 'base64');
-    }
-
-    const { ops } = await dbClient.createFile({
+    const fileData = {
       userId: user._id,
       name,
       type,
       parentId,
       isPublic,
-    });
+    };
+
+    if (type !== 'folder') {
+      fs.mkdirSync(FOLDER_PATH, { recursive: true });
+      const filePath = `${FOLDER_PATH}/${uuidv4()}`;
+      fs.writeFileSync(filePath, data, 'base64');
+      fileData.localPath = filePath;
+
+      // Add thumbnail generation job to the queue
+      fileQueue.add({
+        userId: user._id.toString(),
+        fileId: fileData._id.toString(),
+      });
+    }
+
+    const { ops } = await dbClient.createFile(fileData);
 
     return res.status(201).send({
       id: ops[0]._id,
@@ -68,51 +86,113 @@ class FilesController {
     });
   }
 
-  static async getShow(req, res) {
+  static async putPublish(req, res) {
     const apiToken = req.header('X-Token');
     const userId = await redisClient.get(`auth_${apiToken}`);
 
     if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
     const user = await dbClient.getUser({ _id: ObjectId(userId) });
 
     if (!user) return res.status(401).send({ error: 'Unauthorized' });
 
     const file = await dbClient.getFile({
-      _id: ObjectId(req.params.id),
+      _id: ObjectId(fileId),
       userId: user._id,
     });
 
     if (!file) return res.status(404).send({ error: 'Not found' });
+
+    file.isPublic = true;
+    await dbClient.updateFile(fileId, { isPublic: true });
 
     return res.status(200).send({
       id: file._id,
       userId: file.userId,
       name: file.name,
       type: file.type,
-      isPublic: file.isPublic,
+      isPublic: true,
+      parentId: file.parentId,
     });
   }
 
-  static async getIndex(req, res) {
+  static async putUnpublish(req, res) {
     const apiToken = req.header('X-Token');
     const userId = await redisClient.get(`auth_${apiToken}`);
 
     if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
     const user = await dbClient.getUser({ _id: ObjectId(userId) });
 
     if (!user) return res.status(401).send({ error: 'Unauthorized' });
 
-    const { parentId = 0 } = req.query;
+    const file = await dbClient.getFile({
+      _id: ObjectId(fileId),
+      userId: user._id,
+    });
 
-    const parent = await dbClient.getFile({ _id: parentId });
+    if (!file) return res.status(404).send({ error: 'Not found' });
 
-    if (!parent || parent.type !== 'folder') {
-      return res.status(200).send([]);
+    file.isPublic = false;
+    await dbClient.updateFile(fileId, { isPublic: false });
+
+    return res.status(200).send({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: false,
+      parentId: file.parentId,
+    });
+  }
+
+  static async getFile(req, res) {
+    const apiToken = req.header('X-Token');
+    const userId = await redisClient.get(`auth_${apiToken}`);
+
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
+    const user = await dbClient.getUser({ _id: ObjectId(userId) });
+
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    const file = await dbClient.getFile({
+      _id: ObjectId(fileId),
+      userId: user._id,
+    });
+
+    if (!file) return res.status(404).send({ error: 'Not found' });
+
+    if (!file.isPublic && (file.userId.toString() !== userId)) {
+      return res.status(404).send({ error: 'Not found' });
     }
 
-    const files = await dbClient.getFiles({ parentId, userId: user._id });
+    if (file.type === 'folder') {
+      return res.status(400).send({ error: "A folder doesn't have content" });
+    }
 
-    return res.status(200).send(files);
+    const filePath = file.localPath;
+
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).send({ error: 'Not found' });
+    }
+
+    if (req.query.size) {
+      const thumbnailPath = `${filePath}_${req.query.size}`;
+      if (!fs.existsSync(thumbnailPath)) {
+        return res.status(404).send({ error: 'Not found' });
+      }
+      const mimeType = mime.lookup(thumbnailPath);
+      res.setHeader('Content-Type', mimeType);
+      return fs.createReadStream(thumbnailPath).pipe(res);
+    }
+
+    const mimeType = mime.lookup(filePath);
+    res.setHeader('Content-Type', mimeType);
+    return fs.createReadStream(filePath).pipe(res);
   }
 }
 
--- controllers/dist/FilesController.dev.js
+++ controllers/dist/FilesController.dev.js
@@ -9,7 +9,13 @@ var _uuid = require("uuid");
 
 var _fs = _interopRequireDefault(require("fs"));
 
+var _mimeTypes = _interopRequireDefault(require("mime-types"));
+
 var _mongodb = require("mongodb");
+
+var _imageThumbnail = _interopRequireDefault(require("image-thumbnail"));
+
+var _bull = _interopRequireDefault(require("bull"));
 
 var _db = _interopRequireDefault(require("../utils/db"));
 
@@ -22,6 +28,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+// Create Bull queue
+var fileQueue = new _bull["default"]('fileQueue');
 
 var FilesController =
 /*#__PURE__*/
@@ -38,7 +47,7 @@ function () {
   }, {
     key: "postUpload",
     value: function postUpload(req, res) {
-      var FOLDER_PATH, apiToken, userId, user, _req$body, name, type, _req$body$parentId, parentId, _req$body$isPublic, isPublic, data, parent, _ref, ops;
+      var FOLDER_PATH, apiToken, userId, user, _req$body, name, type, _req$body$parentId, parentId, _req$body$isPublic, isPublic, data, parent, fileData, filePath, _ref, ops;
 
       return regeneratorRuntime.async(function postUpload$(_context) {
         while (1) {
@@ -155,24 +164,35 @@ function () {
               }));
 
             case 29:
-              if (type !== 'folder') {
-                _fs["default"].mkdirSync(FOLDER_PATH, {
-                  recursive: true
-                });
-
-                _fs["default"].writeFileSync("".concat(FOLDER_PATH, "/").concat((0, _uuid.v4)()), data, 'base64');
-              }
-
-              _context.next = 32;
-              return regeneratorRuntime.awrap(_db["default"].createFile({
+              fileData = {
                 userId: user._id,
                 name: name,
                 type: type,
                 parentId: parentId,
                 isPublic: isPublic
-              }));
+              };
 
-            case 32:
+              if (type !== 'folder') {
+                _fs["default"].mkdirSync(FOLDER_PATH, {
+                  recursive: true
+                });
+
+                filePath = "".concat(FOLDER_PATH, "/").concat((0, _uuid.v4)());
+
+                _fs["default"].writeFileSync(filePath, data, 'base64');
+
+                fileData.localPath = filePath; // Add thumbnail generation job to the queue
+
+                fileQueue.add({
+                  userId: user._id.toString(),
+                  fileId: fileData._id.toString()
+                });
+              }
+
+              _context.next = 33;
+              return regeneratorRuntime.awrap(_db["default"].createFile(fileData));
+
+            case 33:
               _ref = _context.sent;
               ops = _ref.ops;
               return _context.abrupt("return", res.status(201).send({
@@ -183,7 +203,7 @@ function () {
                 isPublic: ops[0].isPublic
               }));
 
-            case 35:
+            case 36:
             case "end":
               return _context.stop();
           }
@@ -191,10 +211,10 @@ function () {
       });
     }
   }, {
-    key: "getShow",
-    value: function getShow(req, res) {
-      var apiToken, userId, user, file;
-      return regeneratorRuntime.async(function getShow$(_context2) {
+    key: "putPublish",
+    value: function putPublish(req, res) {
+      var apiToken, userId, fileId, user, file;
+      return regeneratorRuntime.async(function putPublish$(_context2) {
         while (1) {
           switch (_context2.prev = _context2.next) {
             case 0:
@@ -215,16 +235,17 @@ function () {
               }));
 
             case 6:
-              _context2.next = 8;
+              fileId = req.params.id;
+              _context2.next = 9;
               return regeneratorRuntime.awrap(_db["default"].getUser({
                 _id: (0, _mongodb.ObjectId)(userId)
               }));
 
-            case 8:
+            case 9:
               user = _context2.sent;
 
               if (user) {
-                _context2.next = 11;
+                _context2.next = 12;
                 break;
               }
 
@@ -232,18 +253,18 @@ function () {
                 error: 'Unauthorized'
               }));
 
-            case 11:
-              _context2.next = 13;
+            case 12:
+              _context2.next = 14;
               return regeneratorRuntime.awrap(_db["default"].getFile({
-                _id: (0, _mongodb.ObjectId)(req.params.id),
+                _id: (0, _mongodb.ObjectId)(fileId),
                 userId: user._id
               }));
 
-            case 13:
+            case 14:
               file = _context2.sent;
 
               if (file) {
-                _context2.next = 16;
+                _context2.next = 17;
                 break;
               }
 
@@ -251,16 +272,24 @@ function () {
                 error: 'Not found'
               }));
 
-            case 16:
+            case 17:
+              file.isPublic = true;
+              _context2.next = 20;
+              return regeneratorRuntime.awrap(_db["default"].updateFile(fileId, {
+                isPublic: true
+              }));
+
+            case 20:
               return _context2.abrupt("return", res.status(200).send({
                 id: file._id,
                 userId: file.userId,
                 name: file.name,
                 type: file.type,
-                isPublic: file.isPublic
+                isPublic: true,
+                parentId: file.parentId
               }));
 
-            case 17:
+            case 21:
             case "end":
               return _context2.stop();
           }
@@ -268,11 +297,10 @@ function () {
       });
     }
   }, {
-    key: "getIndex",
-    value: function getIndex(req, res) {
-      var apiToken, userId, user, _req$query$parentId, parentId, parent, files;
-
-      return regeneratorRuntime.async(function getIndex$(_context3) {
+    key: "putUnpublish",
+    value: function putUnpublish(req, res) {
+      var apiToken, userId, fileId, user, file;
+      return regeneratorRuntime.async(function putUnpublish$(_context3) {
         while (1) {
           switch (_context3.prev = _context3.next) {
             case 0:
@@ -293,16 +321,17 @@ function () {
               }));
 
             case 6:
-              _context3.next = 8;
+              fileId = req.params.id;
+              _context3.next = 9;
               return regeneratorRuntime.awrap(_db["default"].getUser({
                 _id: (0, _mongodb.ObjectId)(userId)
               }));
 
-            case 8:
+            case 9:
               user = _context3.sent;
 
               if (user) {
-                _context3.next = 11;
+                _context3.next = 12;
                 break;
               }
 
@@ -310,37 +339,174 @@ function () {
                 error: 'Unauthorized'
               }));
 
-            case 11:
-              _req$query$parentId = req.query.parentId, parentId = _req$query$parentId === void 0 ? 0 : _req$query$parentId;
+            case 12:
               _context3.next = 14;
               return regeneratorRuntime.awrap(_db["default"].getFile({
-                _id: parentId
+                _id: (0, _mongodb.ObjectId)(fileId),
+                userId: user._id
               }));
 
             case 14:
-              parent = _context3.sent;
+              file = _context3.sent;
 
-              if (!(!parent || parent.type !== 'folder')) {
+              if (file) {
                 _context3.next = 17;
                 break;
               }
 
-              return _context3.abrupt("return", res.status(200).send([]));
-
-            case 17:
-              _context3.next = 19;
-              return regeneratorRuntime.awrap(_db["default"].getFiles({
-                parentId: parentId,
-                userId: user._id
+              return _context3.abrupt("return", res.status(404).send({
+                error: 'Not found'
               }));
 
-            case 19:
-              files = _context3.sent;
-              return _context3.abrupt("return", res.status(200).send(files));
+            case 17:
+              file.isPublic = false;
+              _context3.next = 20;
+              return regeneratorRuntime.awrap(_db["default"].updateFile(fileId, {
+                isPublic: false
+              }));
+
+            case 20:
+              return _context3.abrupt("return", res.status(200).send({
+                id: file._id,
+                userId: file.userId,
+                name: file.name,
+                type: file.type,
+                isPublic: false,
+                parentId: file.parentId
+              }));
 
             case 21:
             case "end":
               return _context3.stop();
+          }
+        }
+      });
+    }
+  }, {
+    key: "getFile",
+    value: function getFile(req, res) {
+      var apiToken, userId, fileId, user, file, filePath, thumbnailPath, _mimeType, mimeType;
+
+      return regeneratorRuntime.async(function getFile$(_context4) {
+        while (1) {
+          switch (_context4.prev = _context4.next) {
+            case 0:
+              apiToken = req.header('X-Token');
+              _context4.next = 3;
+              return regeneratorRuntime.awrap(_redis["default"].get("auth_".concat(apiToken)));
+
+            case 3:
+              userId = _context4.sent;
+
+              if (userId) {
+                _context4.next = 6;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(401).send({
+                error: 'Unauthorized'
+              }));
+
+            case 6:
+              fileId = req.params.id;
+              _context4.next = 9;
+              return regeneratorRuntime.awrap(_db["default"].getUser({
+                _id: (0, _mongodb.ObjectId)(userId)
+              }));
+
+            case 9:
+              user = _context4.sent;
+
+              if (user) {
+                _context4.next = 12;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(401).send({
+                error: 'Unauthorized'
+              }));
+
+            case 12:
+              _context4.next = 14;
+              return regeneratorRuntime.awrap(_db["default"].getFile({
+                _id: (0, _mongodb.ObjectId)(fileId),
+                userId: user._id
+              }));
+
+            case 14:
+              file = _context4.sent;
+
+              if (file) {
+                _context4.next = 17;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(404).send({
+                error: 'Not found'
+              }));
+
+            case 17:
+              if (!(!file.isPublic && file.userId.toString() !== userId)) {
+                _context4.next = 19;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(404).send({
+                error: 'Not found'
+              }));
+
+            case 19:
+              if (!(file.type === 'folder')) {
+                _context4.next = 21;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(400).send({
+                error: "A folder doesn't have content"
+              }));
+
+            case 21:
+              filePath = file.localPath;
+
+              if (_fs["default"].existsSync(filePath)) {
+                _context4.next = 24;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(404).send({
+                error: 'Not found'
+              }));
+
+            case 24:
+              if (!req.query.size) {
+                _context4.next = 31;
+                break;
+              }
+
+              thumbnailPath = "".concat(filePath, "_").concat(req.query.size);
+
+              if (_fs["default"].existsSync(thumbnailPath)) {
+                _context4.next = 28;
+                break;
+              }
+
+              return _context4.abrupt("return", res.status(404).send({
+                error: 'Not found'
+              }));
+
+            case 28:
+              _mimeType = _mimeTypes["default"].lookup(thumbnailPath);
+              res.setHeader('Content-Type', _mimeType);
+              return _context4.abrupt("return", _fs["default"].createReadStream(thumbnailPath).pipe(res));
+
+            case 31:
+              mimeType = _mimeTypes["default"].lookup(filePath);
+              res.setHeader('Content-Type', mimeType);
+              return _context4.abrupt("return", _fs["default"].createReadStream(filePath).pipe(res));
+
+            case 34:
+            case "end":
+              return _context4.stop();
           }
         }
       });
